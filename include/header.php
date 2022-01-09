ee96debf26ceeb063d63a29589a61444c28988f4
unknown <Enineer-Hasibur-Rahman>
2022-01-09 19:21:40 +0000

    last about page

--- include/header.php
+++ include/header.php
@@ -174,7 +174,7 @@
                                               <li class="menu-item-has-children"> <a href="#">Rankings</a>
                                                 <ul class="sub-menu">
                                                 <li><a href="about2.html">QS World University Rankings</a></li>
-                                                <li><a href="blog.html">QS World University Rankings by Subject</a></li>
+                                                <li><a href="blog.php">QS World University Rankings by Subject</a></li>
                                                 </ul>
                                             </li>
                                             <!--Rankings Menu End--> 
@@ -214,7 +214,130 @@
                                                 <li> <a href="about.html">SOP</a></li>
                                                 <li><a href="about2.html">LOR</a></li>
                                                 <li><a href="blog-details.html">Sending Application</a></li>
-                                                <li><a href="blog-details.html">Acceptance Letter</a></li>
+                  <div class="blog-page-area sec-spacer">
+			<div class="container">
+				<div class="row mb-50 blog-inner">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-images">
+                            <a href="blog-details.html"><i class="fa fa-link" aria-hidden="true"></i> <img src="images/blogpage/1.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-content">
+                            <div class="date">
+                                <i class="fa fa-calendar-check-o"></i> 13 Sep 2017
+                            </div>
+                            <h4><a href="blog-details.html">Traditional Elementary Education of Traditional College</a></h4>
+                            <ul class="blog-meta">
+                                <li class="time"><a href="#"><i class="fa fa-user-o"></i> Admin</a></li>
+                                <li class="date"><i class="fa fa-comments-o"></i> 05</li>
+                            </ul>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum</p>
+                            <a class="primary-btn" href="blog-details.html">Read More</a>
+                        </div> 
+                    </div>
+				</div><!-- .blog-inner end -->
+				<div class="row mb-50 blog-inner">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-images">
+                            <a href="blog-details.html"><i class="fa fa-link" aria-hidden="true"></i> <img src="images/blogpage/2.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-content">
+                            <div class="date">
+                                <i class="fa fa-calendar-check-o"></i> 13 Sep 2017
+                            </div>
+                            <h4><a href="blog-details.html">Traditional Elementary Education of Traditional College</a></h4>
+                            <ul class="blog-meta">
+                                <li class="time"><a href="#"><i class="fa fa-user-o"></i> Admin</a></li>
+                                <li class="date"><i class="fa fa-comments-o"></i> 05</li>
+                            </ul>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum</p>
+                            <a class="primary-btn" href="blog-details.html">Read More</a>
+                        </div> 
+                    </div>
+				</div><!-- .blog-inner end -->
+				<div class="row mb-50 blog-inner">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-images">
+                            <a href="blog-details.html"><i class="fa fa-link" aria-hidden="true"></i> <img src="images/blogpage/3.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-content">
+                            <div class="date">
+                                <i class="fa fa-calendar-check-o"></i> 13 Sep 2017
+                            </div>
+                            <h4><a href="blog-details.html">Traditional Elementary Education of Traditional College</a></h4>
+                            <ul class="blog-meta">
+                                <li class="time"><a href="#"><i class="fa fa-user-o"></i> Admin</a></li>
+                                <li class="date"><i class="fa fa-comments-o"></i> 05</li>
+                            </ul>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum</p>
+                            <a class="primary-btn" href="blog-details.html">Read More</a>
+                        </div> 
+                    </div>
+				</div><!-- .blog-inner end -->
+				<div class="row mb-50 blog-inner">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-images">
+                            <a href="blog-details.html"><i class="fa fa-link" aria-hidden="true"></i> <img src="images/blogpage/4.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-content">
+                            <div class="date">
+                                <i class="fa fa-calendar-check-o"></i> 13 Sep 2017
+                            </div>
+                            <h4><a href="blog-details.html">Traditional Elementary Education of Traditional College</a></h4>
+                            <ul class="blog-meta">
+                                <li class="time"><a href="#"><i class="fa fa-user-o"></i> Admin</a></li>
+                                <li class="date"><i class="fa fa-comments-o"></i> 05</li>
+                            </ul>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum</p>
+                            <a class="primary-btn" href="blog-details.html">Read More</a>
+                        </div> 
+                    </div>
+				</div><!-- .blog-inner end -->
+				<div class="row mb-50 blog-inner">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-images">
+                            <a href="blog-details.html"><i class="fa fa-link" aria-hidden="true"></i> <img src="images/blogpage/5.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="blog-content">
+                            <div class="date">
+                                <i class="fa fa-calendar-check-o"></i> 13 Sep 2017
+                            </div>
+                            <h4><a href="blog-details.html">Traditional Elementary Education of Traditional College</a></h4>
+                            <ul class="blog-meta">
+                                <li class="time"><a href="#"><i class="fa fa-user-o"></i> Admin</a></li>
+                                <li class="date"><i class="fa fa-comments-o"></i> 05</li>
+                            </ul>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum</p>
+                            <a class="primary-btn" href="blog-details.html">Read More</a>
+                        </div> 
+                    </div>
+				</div><!-- .blog-inner end -->
+                <div class="row">
+                    <div class="col-sm-12 col-xs-12">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item disabled"><a class="page-link fa fa-angle-left" href="#" tabindex="-1"></a></li>
+                                <li class="page-item"><a class="page-link active" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link dotted" href="#">...</a></li>
+                                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                <li class="page-item"><a class="page-link" href="#">6</a></li>
+                                <li class="page-item"><a class="page-link fa fa-angle-right" href="#"></a></li>
+                            </ul>
+                        </nav><!-- .navigation end -->
+                    </div>
+                </div>
+			</div>
+		</div>                              <li><a href="blog-details.html">Acceptance Letter</a></li>
                                         
                                                                         
                                             </ul>
