54812fd13e38d4dc8df92142e31e386b1cb3f74b
unknown <Enineer-Hasibur-Rahman>
2022-01-24 17:25:45 +0000

    last

---
+++ admission_support.php
@@ -0,0 +1,80 @@
+
+<!-- Header Start -->
+<?php include("./include/header.php"); ?>
+<!-- Header End -->
+
+
+<br>
+<br>
+
+
+
+<div class="body-container-wrapper">
+    <div class="body-container container-fluid">
+
+<div class="row-fluid-wrapper row-depth-1 row-number-1 ">
+<div class="row-fluid ">
+<div class="span12 widget-span widget-type-custom_widget " style="" data-widget-type="custom_widget" data-x="0" data-w="12">
+<div id="hs_cos_wrapper_module_1593417954666125" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_module" style="" data-hs-cos-general-type="widget" data-hs-cos-type="module">
+
+<div class="banner" style="background:url(hubfs/Banner_Image.jpg);">
+  <div class="banner-overlay" style="background-color:rgba(29, 29, 27,0.7);"></div>
+  <div class="container">
+    <div style="padding-top:140px;padding-bottom:100px" class="banner_content">
+      <div class="span6">
+        <h1 style="">Your admission. Simplified.</h1>
+     
+        
+      </div>
+      <div class="span6">
+        <div class="banner_right_data">
+          
+          <span id="hs_cos_wrapper_module_1593417954666125_"
+           class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_form" style="" 
+           data-hs-cos-general-type="widget" data-hs-cos-type="form">
+           <h3 id="hs_cos_wrapper_form_976670488_title" 
+           class="hs_cos_wrapper form-title" data-hs-cos-general-type="widget_field" 
+           data-hs-cos-type="text"></h3>
+          <div id="hs_form_target_form_976670488"></div>
+          </span>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .banner_right_data form input[type="submit"]{
+    color:#000000;
+    background:#f7a70d;
+    border:1px solid #f7a70d;
+    border-radius:4
+  }
+</style>
+</div>
+</div><!--end widget-span -->
+</div><!--end row-->
+</div><!--end row-wrapper -->
+
+
+
+    </div><!--end body -->
+</div><!--end body wrapper -->
+
+
+
+<!-- Footer Start -->
+<?php include("./include/footer.php"); ?>
+<!-- Footer End -->
+
+
+
+
+
+
+
+
+
+
+
